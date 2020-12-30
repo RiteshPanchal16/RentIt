f3e48b4dbd00e42ceae4a7b2ae8b2276de318c01
Ritesh Panchal <riteshpanchal16@gmail.com>
2020-12-30 09:17:11 +0530

    #

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path("",views.index,name="index"),
     path("index",views.index,name="index"),
     path("about",views.about,name="about"),
